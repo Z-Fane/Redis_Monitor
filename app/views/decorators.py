c99f70bd5ec4279c3d50cf56d57b5dad2d9b253a
ZFane <justonezf@gmail.com>
2018-06-05 16:21:35 +0800

    通过类装饰器进行服务器数据检查

---
+++ app/views/decorators.py
@@ -0,0 +1,23 @@
+from functools import wraps
+
+from flask import g
+
+from app.common.rest import RestException
+
+
+class ObjectMustBeExist():
+    def __init__(self,object_class):
+        self.object_class=object_class
+
+    def __call__(self,func):
+        @wraps(func)
+        def wrapper(*args,**kwargs):
+            object_id=kwargs['object_id']
+            if object_id is None:
+                raise RestException(404,'object not exist')
+            obj=self.object_class.query.get(object_id)
+            if obj is None:
+                raise RestException(404,'object not exist')
+            g.instance=obj
+            return func(*args,**kwargs)
+        return wrapper
--- app/views/views.py
+++ app/views/views.py
@@ -1,5 +1,8 @@
-from flask import render_template
+from flask import render_template, request
 from flask.views import MethodView
+
+from app.common.rest import RestView
+from app.models import Server, ServerSchema
 
 
 class IndexView(MethodView):
@@ -10,3 +13,22 @@ class IndexView(MethodView):
         """渲染模板
         """
         return render_template('index.html')
+
+class ServerList(RestView):
+    """Redis 服务器列表
+    """
+    def get(self):
+        servers = Server.query.all()
+        return ServerSchema().dump(servers, many=True).data
+
+    def post(self):
+        """创建 Redis 实例
+        """
+        data = request.get_json()
+        server, errors = ServerSchema().load(data)
+        if errors:
+            return errors, 400
+        server.ping()
+        server.save()
+        return {'ok': True}, 201
+

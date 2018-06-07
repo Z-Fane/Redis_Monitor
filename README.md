7e92f5c3caa7aaafeb2c5ec08c489d7104780f13
ZFane <justonezf@gmail.com>
2018-06-07 14:09:00 +0800

    微信操作redis服务器指令，补充readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Redis-Monitor
-Redis-Monitor是一个Redis 服务器监控工具,实现了 Redis 服务器管理和监控信息获取的 API
+Redis-Monitor是一个Redis 服务器监控工具
+,实现了 Redis 服务器管理和监控信息获取的 API
+并支持链接微信公众号，微信上操作redis服务器的增删改查
 # 功能设计
 RM支持 Redis 服务器的增删改操作，并支持获取具体某一个服务器的监控信息。总结如下：
 
@@ -17,6 +19,10 @@ RM支持 Redis 服务器的增删改操作，并支持获取具体某一个服�
 |更新服务器 | /servers/server_id| PUT|200|404/400|
 |删除服务器 | /servers/server_id| DELETE|204|404|
 |获取服务器监控数据 | /servers/server_id/metrics| GET| 200 | 404/500|
-
+|微信验证与指令|/wx/|GET|200|404/400|
+|微信绑定|/wx/bind/<wx_id>|GET/POST|200|404/400|
+|登录|/login|POST|200|404/400|
+|用户管理|/users/|GET|200|404/400|
+|用户详情|/users/<int:object_id>|POST|200|404/400|
 
 
--- app/wx/handlers.py
+++ app/wx/handlers.py
@@ -6,6 +6,7 @@ from wechatpy.events import SubscribeEvent
 from wechatpy.messages import TextMessage
 
 from app import User
+from app.models import Server
 
 
 class BaseHandler():
@@ -43,5 +44,46 @@ class BindCommandHandler(CommandHandler):
             return create_reply('你已绑定到 %s 用户' % user.name, message)
         url=url_for('api.wx_bind',wx_id=message.source,_external=True)
         return create_reply('请打开%s链接，完成绑定'%url,message)
+class RedisCommandHandler(CommandHandler):
+    command = 'redis'
+    def handle(self,message):
+        # 检查命令
+        if not self.check_match(message):
+            return
+        # 检查绑定
+        user=User.wx_id_user(wx_id=message.source)
+        if user is None:
+            return create_reply('您还未绑定，请先绑定', message)
+        parts = message.content.strip().split(' ')
+        if len(parts) == 1:
+            return create_reply('请输入子命令', message)
+        if parts[1].lower() == 'ls':
+            return create_reply(self.list_servers(), message)
+        elif parts[1].lower() == 'del':
+            return create_reply(self.delete_server(*parts[2:]), message)
+        else:
+            return create_reply('命令暂未实现', message)
+    def list_servers(self):
+        content = ''
+        for server in Server.query:
+            item = '%s %s %s\n' % (server.name, server.host, server.status)
+            content += item
+        if not content:
+            return '还未创建任何 Redis 服务器'
+        return content
+    def delete_server(self,*servers):
+        if len(servers)==0:
+            return '未指定redis服务器名称'
+        result = ''
+        for name in servers:
+            server = Server.query.filter_by(name=name).first()
+            if server:
+                server.delete()
+                result += '成功删除 %s\n' % server.name
+            else:
+                result += '未发现 %s\n'
+        return result
 
-default_handlers = (SubscribeEventHandler,BindCommandHandler)
+
+
+default_handlers = (SubscribeEventHandler,BindCommandHandler,RedisCommandHandler)
